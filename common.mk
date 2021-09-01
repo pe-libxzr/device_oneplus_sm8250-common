ccb9534a745080cffc0bc0b1f606650392bb53c7
LibXZR <i@xzr.moe>
2021-09-01 18:22:22 +0800

    sm8250-common: Introduce charge control HAL
    Signed-off-by: LibXZR <i@xzr.moe>

--- common.mk
+++ common.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.libxzr.chgctrl@1.0-service.oneplus_kona
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     ftm_power_config.sh \
